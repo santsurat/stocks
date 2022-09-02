28e828dfb25ef93a1f2dc4c20b625d3c93dbfaa8
sant surat <santsurat123@gmail.com>
2022-09-02 11:10:50 +0530

    index.html
    updated

---
+++ main.js
@@ -0,0 +1,45 @@
+ let initialPrice = document.querySelector("#Initial-price");
+ let quantityOfStocks = document.querySelector("#Stocks-Quantity");
+ let currentPrice = document.querySelector("#Current-price");
+ let clickbutton = document.querySelector("#clicked");
+ let outputbox = document.querySelector("#output-box");
+ 
+
+clickbutton.addEventListener("click" , clicked );
+
+function clicked(){
+var actual = Number(initialPrice.value)
+var qty =  Number(quantityOfStocks.value)
+var curr = Number(currentPrice.value)
+
+ CalculateProfitAndLoss(actual,qty,curr);
+}
+debugger;
+function CalculateProfitAndLoss(initial,quantity,Current) {
+
+        if(initial > Current){
+           var loss = (initial - Current) * quantity;
+           var lossPercentage = (loss / initial) * 100;
+           ShowOutput(`Hey the profit is ${loss}and the percentage is ${lossPercentage}%`);
+        }else if(Current > initial){
+            var profit = ( Current - initial ) * quantity;
+            var profitPercentage = (profit / initial) * 100;
+
+           ShowOutput(`Hey the profit is ${profit}and the percentage is ${profitPercentage}%`);
+        }else{
+           ShowOutput(`no pain no gain and no gain no pain`);
+        }
+};
+
+function ShowOutput(message) {
+
+    outputbox.innerHTML = message;
+    outputbox.style.color = 'green';
+    
+}
+
+
+
+
+
+
